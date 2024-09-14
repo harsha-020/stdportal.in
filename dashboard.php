f607df28f5ce9befd65c3504329e184c8b9cc59f
harsha-020 <161411404+harsha-020@users.noreply.github.com>
2024-09-14 13:12:06 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,96 @@
+<?php
+    include("init.php");
+    
+    $no_of_classes=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(*) FROM `class`"));
+    $no_of_students=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(*) FROM `students`"));
+    $no_of_result=mysqli_fetch_array(mysqli_query($conn,"SELECT COUNT(*) FROM `result`"));
+?>
+        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+body{
+background-image:url('home3.jpg');
+background-repeat:no-repeat;
+background-attachment:fixed;
+background-size:100% 100%;
+
+}
+
+</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/home.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="./css/font-awesome-4.7.0/css/font-awesome.css">
+    <link rel="stylesheet" href="normalize.css">
+    <title>Dashboard</title>
+    <style>
+        .main{
+            border-radius: 10px;
+            border-width: 5px;
+            border-style: solid;
+            padding: 20px;
+            margin: 7% 20% 0 20%;
+        }
+    </style>
+</head>
+<body>
+        
+    <div class="title">
+ 	
+        <a href="dashboard.php"><img src="./jpkp.png" alt="" class="logo"></a>
+        <span class="heading">Dashboard</span>
+        <a href="logout.php" style="color: white"><span class="fa fa-sign-out fa-2x">Logout</span></a>
+    </div>
+
+    <div class="nav">
+        <ul>
+            <li class="dropdown" onclick="toggleDisplay('1')">
+                <a href="" class="dropbtn">Classes &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="1">
+                    <a href="add_classes.php">Add Class</a>
+                    <a href="manage_classes.php">Manage Class</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('2')">
+                <a href="#" class="dropbtn">Students &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="2">
+                    <a href="add_students.php">Add Students</a>
+                    <a href="manage_students.php">Manage Students</a>
+                </div>
+            </li>
+            <li class="dropdown" onclick="toggleDisplay('3')">
+                <a href="#" class="dropbtn">Results &nbsp
+                    <span class="fa fa-angle-down"></span>
+                </a>
+                <div class="dropdown-content" id="3">
+                    <a href="add_results.php">Add Results</a>
+                    <a href="manage_results.php">Manage Results</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <div class="main">
+        <?php
+            echo '<b><p style="color:white;">Number of classes:'.$no_of_classes[0].'</p></b>';
+            echo '<b><p style="color:white;">Number of students:'.$no_of_students[0].'</p></b>';
+            echo '<b><p style="color:white;">Number of results:'.$no_of_result[0].'</p></b>';
+        ?>
+    </div>
+
+    <div class="footer">
+        <!-- <span>From CCBD Final Year</span> -->
+    </div>
+</body>
+</html>
+
+<?php
+   include('session.php');
+?>
---
+++ student_result.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<style>
+body{
+background-image:url('home3.jpg');
+background-repeat:no-repeat;
+background-attachment:fixed;
+background-size:100% 100%;
+
+}
+
+</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+<body>
+ <div class="search">
+            <form action="./student.php" method="get">
+                <fieldset>
+                    <legend class="heading">STUDENT RESULTS</legend>
+
+                    <?php
+                        include('init.php');
+
+                        $class_result=mysqli_query($conn,"SELECT `name` FROM `class`");
+                            echo '<select name="class">';
+                            echo '<option selected disabled>Select Class</option>';
+                        while($row = mysqli_fetch_array($class_result)){
+                            $display=$row['name'];
+                            echo '<option value="'.$display.'">'.$display.'</option>';
+                        }
+                        echo'</select>'
+                    ?>
+
+                    <input type="text" name="rn" placeholder="Roll No">
+                    <input type="submit" value="Get Result">
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</body>
